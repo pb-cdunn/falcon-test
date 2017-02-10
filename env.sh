4e4a0d856c20b2c1bc0fa59978235f6a5adf6de6
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-02-09 16:59:21 -0800

    Maybe Bamboo vars go into Bamboo

--- env.sh
+++ env.sh
@@ -2,18 +2,15 @@
 
 export PATH=/mnt/software/a/anaconda2/4.2.0/bin:$PATH
 export PYTHONUSERBASE=$(pwd)/LOCAL
-if [[ -n ${bamboo.planRepository.branch} ]]; then
-  export FALCON_GIT_BRANCH=${bamboo.planRepository.branch}
-else
+#export FALCON_GIT_BRANCH=${bamboo.planRepository.branch}
+#export FALCON_GIT_URL=${bamboo.planRepository.repositoryUrl}
+if [[ -z $FALCON_GIT_BRANCH ]]; then
   export FALCON_GIT_BRANCH=$(git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\///')
 fi
-if [[ ${bamboo.planRepository.repositoryUrl} ]]; then
-    export FALCON_GIT_URL=${bamboo.planRepository.repositoryUrl}
+if [[ -z $FALCON_GIT_URL ]]; then
 else
     export FALCON_GIT_URL=$(git config --get remote.origin.url)
 fi
-#export FALCON_GIT_BRANCH=$(git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\///')
-#export FALCON_GIT_URL=$(git config --get remote.origin.url)
 export FALCON_WORKSPACE=BUILD
 export FALCON_PREFIX=${PYTHONUSERBASE}
 
